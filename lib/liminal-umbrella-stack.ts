877fc7280fc7902092dbdb649f59733dce2d32ac
Tomas Doran <tdoran@yelp.com>
2024-02-11 21:01:13 +0000

    More stuff

--- lib/liminal-umbrella-stack.ts
+++ lib/liminal-umbrella-stack.ts
@@ -1,10 +1,15 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs'
-//import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import {Duration} from 'aws-cdk-lib';
+
 
 export class LiminalUmbrellaStack extends cdk.Stack {
+  public readonly usersTable: dynamodb.TableV2;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -22,8 +27,20 @@ export class LiminalUmbrellaStack extends cdk.Stack {
   code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
 });
 	*/
-     const table = new dynamodb.TableV2(this, 'TestTable', {
+    this.usersTable = new dynamodb.TableV2(this, 'TestTable', {
        partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
     });
+
+    // Create the Lambdas next.
+    const discordCommandsLambda = new lambda.Function(this, 'discord-commands-lambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, 'functions')),
+      handler: 'handler',
+      environment: {
+        USERS_TABLE_NAME: this.usersTable.tableName,
+      },
+      timeout: Duration.seconds(60),
+    });
+    this.usersTable.grantReadWriteData(discordCommandsLambda);
   }
 }
