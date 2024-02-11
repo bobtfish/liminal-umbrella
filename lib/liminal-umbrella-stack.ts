1afa85af1fbb625d8bffcc12cedc2bcf9f5d1b92
Tomas Doran <tdoran@yelp.com>
2024-02-11 20:32:48 +0000

    dynamodb

--- lib/liminal-umbrella-stack.ts
+++ lib/liminal-umbrella-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs'
+//import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as dynamodb from 'aws-cdk-lib/aws_dynamodb'
 
 export class LiminalUmbrellaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +14,16 @@ export class LiminalUmbrellaStack extends cdk.Stack {
     /*const queue = new sqs.Queue(this, 'LiminalUmbrellaQueue', {
        visibilityTimeout: cdk.Duration.seconds(300)
     });*/
+
+    /*
+	const fn = new lambda.Function(this, 'MyFunction', {
+  runtime: lambda.Runtime.NODEJS_18_X,
+  handler: 'index.handler',
+  code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+});
+	*/
+     const table = new dynamodb.TableV2(this, 'TestTable', {
+       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+    });
   }
 }
