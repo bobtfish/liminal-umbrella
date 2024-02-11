6651058bcdfc9de8429cf77e977c0df6ba421be4
Tomas Doran <tdoran@yelp.com>
2024-02-11 20:27:42 +0000

    Working

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import { App } from '@aws-cdk/core';
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
 import { LiminalUmbrellaStack } from './stacks/start-api-stack';
 
 const stackEnv = {
@@ -6,8 +7,19 @@ const stackEnv = {
   region: process.env.CDK_DEFAULT_REGION
 };
 
-const app = new App();
-const liminalUmbrellaStack = new LiminalUmbrellaStack(app, 'LiminalUmbrellaStack', {
-  domainAddress: process.env.LAMBDA_DOMAIN,
-  env: stackEnv
+const app = new cdk.App();
+new LiminalUmbrellaStack(app, 'LiminalUmbrellaStack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  env: stackEnv,
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  // env: { account: '123456789012', region: 'us-east-1' },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
