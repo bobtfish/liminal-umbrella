d01f265be4f57fa0e8bf4e600432f01693171acb
Tomas Doran <tdoran@yelp.com>
2024-02-11 20:33:03 +0000

    dynamodb

--- lib/liminal-umbrella-stack.ts
+++ lib/liminal-umbrella-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs'
 //import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as dynamodb from 'aws-cdk-lib/aws_dynamodb'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 
 export class LiminalUmbrellaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
