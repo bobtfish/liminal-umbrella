720c1169c213aa64a07efeb09f455bc82e9943dc
Tomas Doran <tdoran@yelp.com>
2024-02-21 20:25:57 +0000

    Remove slash-commands and do it outselves

--- src/tools/configure-discord-server.ts
+++ src/tools/configure-discord-server.ts
@@ -1,7 +1,7 @@
-import {DiscordInteractions, PartialApplicationCommand} from 'slash-commands';
 import {getDiscordSecrets} from '../utils/DiscordSecrets';
 import * as Stacks from '../configs/outputs.json';
 import { IDiscordSecrets } from '../types';
+import {getApplicationCommands, createApplicationCommand, deleteApplicationCommand} from '../utils/DiscordApplicationCommands';
 
 const commands = [
   {
@@ -23,35 +23,45 @@ const commands = [
   },*/
 ];
 
-console.log(Stacks.LiminalUmbrellaStack.discordSecretName);
+async function listCommands(discordSecrets: IDiscordSecrets) {
+  const existingCommands = await getApplicationCommands(discordSecrets).catch(console.error) || [];
+  for (const command of existingCommands) {
+    console.log(command.name + ": " + command.description);
+  }
+}
+
+async function setupCommands(discordSecrets: IDiscordSecrets) {
+  for (const command of commands) {
+    await createApplicationCommand(discordSecrets, command).then((foo : any) => {
+      console.log(`Created command ${command.name}!`);
+    }).catch(console.error);
+  }
+}
+
+async function resetCommands(discordSecrets: IDiscordSecrets) {
+  const existingCommands = await getApplicationCommands(discordSecrets).catch(console.error) || [];
+  for (const command of existingCommands) {
+    await deleteApplicationCommand(discordSecrets, command.id)
+        .then(() => {
+          console.log(`Deleted command ${command.name}!`);
+        })
+        .catch(console.error);
+  };
+}
+
 getDiscordSecrets(Stacks.LiminalUmbrellaStack.discordSecretName).then(async (discordSecrets : IDiscordSecrets | undefined) : Promise<any> => {
   if (discordSecrets) {
-    const interaction = new DiscordInteractions({
-      applicationId: discordSecrets.applicationId,
-      authToken: discordSecrets.authToken,
-      publicKey: discordSecrets.publicKey,
-    });
-
     const inputArgs = process.argv.slice(2);
 
     switch (inputArgs[0]) {
+      case 'list':
+        await listCommands(discordSecrets);
+        break;
       case 'setup':
-        for (const command of commands) {
-          await interaction.createApplicationCommand(command).then(() => {
-            console.log(`Created command ${command.name}!`);
-          }).catch(console.log);
-        }
+        await setupCommands(discordSecrets);
         break;
       case 'reset':
-        const existingCommands = await interaction.getApplicationCommands();
-        for (const command of existingCommands) {
-          await interaction
-              .deleteApplicationCommand(command.id)
-              .then(() => {
-                console.log(`Deleted command ${command.name}!`);
-              })
-              .catch(console.error);
-        };
+        await resetCommands(discordSecrets)
         break;
     }
   } else {
--- src/types.ts
+++ src/types.ts
@@ -106,3 +106,105 @@ export interface IDiscordResponseData {
   embeds: any[];
   allowedMentions: string[];
 }
+
+export type ApplicationCommandOptionChoice = {
+  name: string;
+  value: string | number;
+};
+
+export type ApplicationCommandOption = {
+  type: ApplicationCommandOptionType;
+  name: string;
+  description: string;
+  default?: boolean;
+  required?: boolean;
+  choices?: ApplicationCommandOptionChoice[];
+  options?: ApplicationCommandOption[];
+};
+
+export type ApplicationCommandOptionValue = string | number | boolean;
+
+export enum ApplicationCommandOptionType {
+  SUB_COMMAND = 1,
+  SUB_COMMAND_GROUP = 2,
+  STRING = 3,
+  INTEGER = 4,
+  BOOLEAN = 5,
+  USER = 6,
+  CHANNEL = 7,
+  ROLE = 8,
+}
+
+export type ApplicationCommand = {
+  id: string;
+  application_id: string;
+  name: string;
+  description: string;
+  options: ApplicationCommandOption[];
+};
+
+export type PartialApplicationCommand = {
+  name: string;
+  description: string;
+  options?: ApplicationCommandOption[];
+};
+
+export type Embed = {
+  title?: string;
+  // type?: string; // deprecated
+  description?: string;
+  url?: string;
+  timestamp?: Date;
+  color?: number;
+  footer?: EmbedFooter;
+  image?: EmbedImage;
+  thumbnail?: EmbedThumbnail;
+  video?: EmbedVideo;
+  provider?: EmbedProvider;
+  author?: EmbedAuthor;
+  fields?: EmbedField[];
+};
+
+export type EmbedThumbnail = {
+  url?: string;
+  proxyUrl?: string;
+  height?: number;
+  width?: number;
+};
+
+export type EmbedVideo = {
+  url?: string;
+  height?: number;
+  width?: number;
+};
+
+export type EmbedImage = {
+  url?: string;
+  proxyUrl?: string;
+  height?: number;
+  width?: number;
+};
+
+export type EmbedProvider = {
+  name?: string;
+  url?: string;
+};
+
+export type EmbedAuthor = {
+  name?: string;
+  url?: string;
+  iconUrl?: string;
+  proxyIconUrl?: string;
+};
+
+export type EmbedFooter = {
+  text?: string;
+  iconUrl?: string;
+  proxyIconUrl?: string;
+};
+
+export type EmbedField = {
+  name: string;
+  value: string;
+  inline?: boolean;
+};
