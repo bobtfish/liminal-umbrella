ed001a44ea867b303685482820df3708619e9dbb
Tomas Doran <tdoran@yelp.com>
2024-02-23 22:09:59 +0000

    Add fly

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -3,10 +3,22 @@ process.env.NODE_ENV ??= 'development';
 
 import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
 import '@sapphire/plugin-logger/register';
-import { setup } from '@skyra/env-utilities';
 import * as colorette from 'colorette';
 import { join } from 'node:path';
 import { srcDir } from './constants';
+
+import type { IntegerString } from '@skyra/env-utilities';
+import { setup } from '@skyra/env-utilities';
+
+
+declare module '@skyra/env-utilities' {
+	interface Env {
+		DISCORD_TOKEN: string;
+        DISCORD_BOT_TOKEN: string;
+        DISCORD_APPLICATION_ID: IntegerString;
+        DISCORD_PUBLIC_KEY: string;
+	}
+}
 
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
