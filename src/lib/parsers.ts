504094729c79320d9548a659ee35b5ca88592316
Tomas Doran <tdoran@yelp.com>
2024-03-08 18:58:55 +0000

    Dates

--- src/lib/parsers.ts
+++ src/lib/parsers.ts
@@ -12,6 +12,7 @@ export type AGameSession = {
 
 const nameRe = new RegExp(/(?:\*\*)?(?:Adventure [nN]ame|Game)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
 const typeRe = new RegExp(/(?:\*\*)?(?:Type of Adventure)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
+const dateRe = new RegExp(/(?:\*\*)?(?:(?:Start )?[dD]ate)\s*(?:\*\*)?:\s*(?:\*\*)?\s*(?:\w+day)?\s*(\d{1,2}\/\d{1,2}\/\d{2,4})/);
 
 export function parseAvailableGame(input: string) : AGameSession {
   const out = {
@@ -32,6 +33,10 @@ export function parseAvailableGame(input: string) : AGameSession {
   const typeMatches = input.match(typeRe);
   if (typeMatches) {
     out["type"] = typeMatches[1];
+  }
+  const dateMatches = input.match(dateRe);
+  if (dateMatches) {
+    out["date"] = dateMatches[1];
   }
   return out;
 }
