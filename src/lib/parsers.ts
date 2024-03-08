dc0f1acb387641547fe072afcb9e86ff047503db
Tomas Doran <tdoran@yelp.com>
2024-03-08 19:26:26 +0000

    Dm

--- src/lib/parsers.ts
+++ src/lib/parsers.ts
@@ -10,11 +10,15 @@ export type AGameSession = {
   description: string;
 };
 
+const inputRe = new RegExp(/\s\s\s+/);
 const nameRe = new RegExp(/(?:\*\*)?(?:Adventure [nN]ame|Game)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
 const typeRe = new RegExp(/(?:\*\*)?(?:Type of Adventure)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
 const dateRe = new RegExp(/(?:\*\*)?(?:(?:Start )?[dD]ate)\s*(?:\*\*)?:\s*(?:\*\*)?\s*(?:\w+day)?\s*(\d{1,2}\/\d{1,2}\/\d{2,4})/);
+const dmRe = new RegExp(/(?:\*\*)?(?:DM [cC]ontact|DM)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
+
 
 export function parseAvailableGame(input: string) : AGameSession {
+  input = input.replace(inputRe, "\n");
   const out = {
     name: "??",
     type: "??",
@@ -37,6 +41,10 @@ export function parseAvailableGame(input: string) : AGameSession {
   const dateMatches = input.match(dateRe);
   if (dateMatches) {
     out["date"] = dateMatches[1];
+  }
+  const dmMatches = input.match(dmRe);
+  if (dmMatches) {
+    out["dm_contact"] = dmMatches[1];
   }
   return out;
 }
