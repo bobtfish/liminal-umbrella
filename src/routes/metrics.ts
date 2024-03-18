3ec468265dda2c78ec996b441f46da9333a95a21
Tomas Doran <tdoran@yelp.com>
2024-03-18 21:15:23 +0000

    Add prom endpoint and GC debug

---
+++ src/routes/metrics.ts
@@ -0,0 +1,17 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Route, methods, type ApiRequest, type ApiResponse } from '@sapphire/plugin-api';
+import promClient from 'prom-client'
+
+const register = new promClient.Registry();
+register.setDefaultLabels({
+  app: process.env.FLY_APP_NAME || 'local-test-bot',
+});
+promClient.collectDefaultMetrics({ register });
+
+@ApplyOptions<Route.Options>({ route: 'metrics' })
+export class UserRoute extends Route {
+	public async [methods.GET](_: ApiRequest, response: ApiResponse) {
+		response.setHeader('Content-Type', register.contentType);
+  		return response.respond(await register.metrics());
+	}
+}
