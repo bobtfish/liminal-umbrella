bc9cf9b6d138e275b04d972fd9ccf4527012c341
Tomas Doran <tdoran@yelp.com>
2024-05-04 21:09:42 +0100

    Deletion

---
+++ src/cogs/deleteOldMessages/utils.ts
@@ -0,0 +1,7 @@
+export function getChannelName() : string | null {
+  const channel_name = process.env.DELETE_OLD_MESSAGES_CHANNEL;
+  if (!channel_name) {
+    return null;
+  }
+  return channel_name;
+}
