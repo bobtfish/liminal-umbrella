ff1fe996d0e23f65b3c9cc6ad607203ab85deb35
Tomas Doran <tdoran@yelp.com>
2024-05-06 22:53:42 +0100

    Update to log into botspraff

---
+++ src/cogs/randomActivity/activity.ts
@@ -0,0 +1,15 @@
+import { container } from '@sapphire/framework';
+import { srcDir } from '../../lib/constants.js';
+import { join } from 'node:path';
+import { readFileSync } from 'node:fs';
+import { ActivityType } from 'discord.js';
+import { getChannelAndSend } from '../verboseLog/utils.js';
+
+const fn = join(srcDir, 'cogs', 'randomActivity', 'playing.txt');
+const lines = readFileSync(fn, 'utf-8').split(/\r?\n/);
+
+export function setRandomActivity() {
+    const thing = lines[Math.floor(Math.random()*lines.length)];
+    getChannelAndSend(`Set activity: Playing ${thing}`);
+    container.client.user?.setActivity({ name: thing, type: ActivityType.Playing });
+}
