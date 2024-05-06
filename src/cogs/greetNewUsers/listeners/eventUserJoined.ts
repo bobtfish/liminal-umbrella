0c1b60c229c09aef43e48711b60b85bf951f643f
Tomas Doran <tdoran@yelp.com>
2024-05-06 19:49:38 +0100

    Fix text

--- src/cogs/greetNewUsers/listeners/eventUserJoined.ts
+++ src/cogs/greetNewUsers/listeners/eventUserJoined.ts
@@ -15,7 +15,7 @@ export class GreetNewUsersUserJoinedListener extends Listener {
 
   @Sequential
   async run (e: UserJoined) {
-    const id = await getChannelAndSend(this.container, `<@${e.id}> Welcome to Preston DnD 5e community.  If you have read the rules and altered your name as asked then an Admin will be along shortly to give access to the rest of the server.`);
+    const id = await getChannelAndSend(this.container, `<@${e.id}> Welcome to Preston RPG Community.  If you have read the rules and altered your name as asked then an Admin will be along shortly to give access to the rest of the server.`);
     if (id) {
       await this.container.database.greetingMessageAdd(id, e.id);
     }
