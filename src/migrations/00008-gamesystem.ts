e937c2378f7a9771d0b559aa3bf1d78c5a8bad33
Tomas Doran <tdoran@yelp.com>
2024-06-08 21:58:53 +0100

    More systems

--- src/migrations/00008-gamesystem.ts
+++ src/migrations/00008-gamesystem.ts
@@ -37,19 +37,91 @@ export const up = async (uz: MigrationParams<any>) => {
             name: 'dnd5e',
             description: 'DnD 5e',
             createdAt: new Date(),
-            updatedAt: new Date()
+            updatedAt: new Date(),
+        },
+        {
+            name: 'dnd3e',
+            description: 'DnD 3e',
+            createdAt: new Date(),
+            updatedAt: new Date(),
         },
         {
             name: 'pathfinder',
             description: 'Pathfinder 2e',
             createdAt: new Date(),
-            updatedAt: new Date()
+            updatedAt: new Date(),
+        },
+        {
+            name: 'knave2e',
+            description: 'Knave 2e (DnD 1e OSR)',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'dcc',
+            description: 'Dungeon Crawl Classics (DnD 1e OSR)',
+            createdAt: new Date(),
+            updatedAt: new Date(),
         },
         {
             name: 'alien',
-            description: 'Alien',
+            description: 'Alien RPG (Year Zero)',
             createdAt: new Date(),
-            updatedAt: new Date()
+            updatedAt: new Date(),
+        },
+        {
+            name: 'coriolis',
+            description: 'Coriolis (Year Zero)',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'forvbiden-lands',
+            description: 'Forbidden Lands (Year Zero)',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'fallout',
+            description: 'Fallout RPG',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'cthulu',
+            description: 'Call of Cthulhu',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'delta-green',
+            description: 'Delta Green',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'whitehack',
+            description: 'Whitehack',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'vtm',
+            description: 'Vampire: The Masquerade (World of Darkness)',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'mausritter',
+            description: 'Mausritter',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'other-d6',
+            description: 'Other d6',
+            createdAt: new Date(),
+            updatedAt: new Date(),
         },
     ]);
 };
