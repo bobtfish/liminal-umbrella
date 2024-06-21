c429b54b80831f3151f3b46ef5eaf403b003c92b
Tomas Doran <tdoran@yelp.com>
2024-06-22 00:13:43 +0100

    Fix

--- frontend/src/Homepage.tsx
+++ frontend/src/Homepage.tsx
@@ -2,25 +2,29 @@ import { LogoutButton, LoginButton, isAuthenticated, AuthContext } from './Auth'
 import './App.css'
 import { useContext } from 'react';
 
-function AuthData() {
+export function AuthData() {
   const auth = useContext(AuthContext);
   if (!auth || !auth.data) {
     return <div>NO AUTH DATA</div>
   }
   return <div className="authdata">
   LOADING: {auth.isFetching ? 'true' : 'false'}<br />
-  Network Error: {auth.isError}<br />
+  FETCH STATUS: {auth.fetchStatus}<br />
+  auth.isFetching: {auth.isFetching? 'true' : 'false'}<br />
+  isSuccess: {auth.isSuccess? 'true' : 'false'}<br />
+  Network Error: {auth.isError? 'true' : 'false'}<br />
   Auth Error: {auth.data.error}<br />
   DATA: <pre>{JSON.stringify(auth.data, null, 2)}</pre><br />
   </div>
 }
 
 function HomePage() {
+  console.log('HomePage')
   return (
     <>
-      <div>
+      <div>IN HOMEPAGE
         <div className="card">
-          <LoginButton />
+          <LoginButton />MOOO
           {isAuthenticated() && <AuthData />}
           <LogoutButton />
         </div>
