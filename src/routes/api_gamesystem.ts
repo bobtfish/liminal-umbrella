370b311daf37bac856374419d5264ad19d36f0d7
Tomas Doran <tdoran@yelp.com>
2024-06-25 20:03:32 +0100

    game system api generic

--- src/routes/api_gamesystem.ts
+++ src/routes/api_gamesystem.ts
@@ -1,37 +1,21 @@
-import { methods, Route, type ApiRequest, type ApiResponse, HttpCodes } from '@sapphire/plugin-api';
+import { Route, } from '@sapphire/plugin-api';
 import { GameSystem } from '../lib/database/model.js';
 import { GameSystemSchema } from "common/schema";
-import { AuthenticatedAdmin } from '../lib/api/decorators.js';
-import {Sequential} from '../lib/utils.js';
+import type { SchemaBundle } from "common/schema"
+import { CR } from '../lib/api/CRUD.js';
 
+export class ApiGaneSystemList extends CR {
+  public constructor(context: Route.LoaderContext, options: Route.Options) {
+    super(context, {
+      ...options,
+      route: 'api/gamesystem'
+    });
+  }
 
-//TODO - Add decorators to require authentication
-export class ApiBotplayingList extends Route {
-    public constructor(context: Route.LoaderContext, options: Route.Options) {
-      super(context, {
-        ...options,
-        route: 'api/gamesystem'
-      });
-    }
-
-    // Get current list
-    @AuthenticatedAdmin()
-    @Sequential
-    public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
-        const gamesystems = await GameSystem.findAll({ where: { }});
-        response.json(gamesystems.map(gamesystem => {return {key: gamesystem.key, name: gamesystem.name, description: gamesystem.description}}))
-    }
-
-    // Add a new one
-    @AuthenticatedAdmin()
-    @Sequential
-    public async [methods.POST](request: ApiRequest, response: ApiResponse) {
-        const { success, error, data } = GameSystemSchema.create.safeParse(request.body);
-        if (!success) {
-            response.status(HttpCodes.BadRequest).json({status: "error", error: error.issues });
-            return;
-        }
-        const gamesystem = await GameSystem.create(data as any);
-        response.status(HttpCodes.Created).json({status: "ok", gamesystem: {key: gamesystem.key, name: gamesystem.name, description: gamesystem.description}});
-    }
+  getModel() {
+    return GameSystem
+  }
+  getSchema(): SchemaBundle {
+    return GameSystemSchema
+  }
 }
--- src/routes/api_gamesystem_edit.ts
+++ src/routes/api_gamesystem_edit.ts
@@ -1,56 +1,21 @@
-import { methods, Route, type ApiRequest, type ApiResponse, HttpCodes } from '@sapphire/plugin-api';
+import { Route } from '@sapphire/plugin-api';
 import { GameSystem } from '../lib/database/model.js';
 import { GameSystemSchema } from "common/schema";
-import { AuthenticatedAdmin } from '../lib/api/decorators.js';
-import { Sequential } from '../lib/utils.js';
+import type { SchemaBundle } from "common/schema"
+import { CR } from '../lib/api/CRUD.js';
 
-export class ApiGameSystemEdit extends Route {
-    public constructor(context: Route.LoaderContext, options: Route.Options) {
-      super(context, {
-        ...options,
-        route: 'api/gamesystem/:key'
-      });
-    }
+export class ApiGaneSystemEdit extends CR {
+  public constructor(context: Route.LoaderContext, options: Route.Options) {
+    super(context, {
+      ...options,
+      route: 'api/gamesystem/:key'
+    });
+  }
 
-    private async findGameSystem(params: ApiRequest['params'], response: ApiResponse): Promise<GameSystem | null> {
-      const { success, error, data } = GameSystemSchema.delete.safeParse(params);
-      if (!success) {
-          response.status(HttpCodes.BadRequest).json({status: "error", error: error.issues });
-          return null;
-      }
-      const activity = await GameSystem.findOne({where: data});
-      if (!activity) {
-          response.status(HttpCodes.NotFound).json({status: "error", error: "Activity not found"});
-          return null;
-      }
-      return activity;
-    }
-
-    @AuthenticatedAdmin()
-    @Sequential
-    public async [methods.POST](request: ApiRequest, response: ApiResponse) {
-      const gamesystem = await this.findGameSystem(request.params, response);
-      if (!gamesystem) {
-          return;
-      }
-      const { success, error, data } = GameSystemSchema.update.safeParse(request.body);
-        if (!success) {
-            response.status(HttpCodes.BadRequest).json({status: "error", error: error.issues });
-            return;
-        }
-        gamesystem.set(data);
-        await gamesystem.save();
-        response.json({status: "ok", activity: data});
-    }
-
-    @AuthenticatedAdmin()
-    @Sequential
-    public async [methods.DELETE](request: ApiRequest, response: ApiResponse) {
-      const gamesystem = await this.findGameSystem(request.params, response);
-      if (!gamesystem) {
-          return;
-      }
-        await gamesystem.destroy();
-        response.json({status: "deleted", gamesystem: request.params});
-    }
+  getModel() {
+    return GameSystem
+  }
+  getSchema(): SchemaBundle {
+    return GameSystemSchema
+  }
 }
