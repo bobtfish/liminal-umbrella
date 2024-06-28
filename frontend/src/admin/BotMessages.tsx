87057f0637120fa3a37a229db1b9cb846a4349aa
Tomas Doran <tdoran@yelp.com>
2024-06-28 09:33:47 +0100

    Add warning

--- frontend/src/admin/BotMessages.tsx
+++ frontend/src/admin/BotMessages.tsx
@@ -1,5 +1,7 @@
 import Table from 'antd/es/table';
 import Spin from 'antd/es/spin';
+import Result from 'antd/es/result';
+import Divider from 'antd/es/divider';
 import { BotMessageSchema } from 'common/schema';
 import { getComponents, ColumnTypes, getQueries, getColumns, DefaultColumns, WrapCRUD } from '../CRUD.js';
 
@@ -27,9 +29,20 @@ export default function AdminBotMessages() {
 
   const columns = getColumns<FetchBotMessageItem>(defaultColumns, handleSave);
 
+  const codeexample = '${this....}'
+  const leftBracket = '${'
+  const rightBracket = '}'
   return <WrapCRUD<FetchBotMessageItem> result={result}>
     <>
       <Spin spinning={isMutating} fullscreen />
+      <Result
+        status="warning"
+        title="Danger - Altering these templates can break the bot."
+        extra={<>The interpolations (things contained inside <code>{codeexample}</code>) are references
+        to specific things inside the bot code. <b>Do not</b>change or edit them unless you know what you are doing.
+        Unbalanced brackets (<code>{leftBracket}</code> without the <code>{rightBracket}</code>) will cause errors.</>}
+      />
+      <Divider />
       <Table
         components={components}
         rowClassName={() => 'editable-row'}
