cc685bc8db859eccc07bc8812d18c9272354d9de
Tomas Doran <t0m@the.internet.never.works>
2024-07-16 22:54:40 +0100

    Do not log health or metrics

--- src/cogs/logApiRequests/middlewares/LogApiRequests.ts
+++ src/cogs/logApiRequests/middlewares/LogApiRequests.ts
@@ -4,6 +4,7 @@ import { getChannelAndSend } from '../../verboseLog/utils.js';
 
 export class LogApiRequests extends Middleware {
 	public override run(request: ApiRequest, _response: ApiResponse) {
+		if (request.url == '/health' || request.url == '/metrics') return;
 		const user = request.auth ? request.auth.nickname : 'ANON';
 		getChannelAndSend(`API request - ${user}: ${request.method} ${request.url}`);
 	}
