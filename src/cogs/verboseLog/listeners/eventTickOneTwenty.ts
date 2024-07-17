c887c1c6a16ac8980bf41427a05945829de623eb
Tomas Doran <t0m@the.internet.never.works>
2024-07-17 07:06:54 +0100

    Format date

--- src/cogs/verboseLog/listeners/eventTickOneTwenty.ts
+++ src/cogs/verboseLog/listeners/eventTickOneTwenty.ts
@@ -1,6 +1,7 @@
 import { Listener, container } from '@sapphire/framework';
 import { TickOneTwenty } from '../../../lib/events/index.js';
 import { getChannelAndSend } from '../utils.js';
+import dayjs from 'dayjs';
 
 export class verboseLogTickOneTwentyListener extends Listener {
 	public constructor(context: Listener.LoaderContext, options: Listener.Options) {
@@ -12,7 +13,8 @@ export class verboseLogTickOneTwentyListener extends Listener {
 		});
 	}
 	run(e: TickOneTwenty) {
-		container.logger.info('verboseLog cog - tickOneTwenty');
-		getChannelAndSend(`Tick (2 hours): ${e.firedAt}`);
+		const thing = dayjs(new Date(e.firedAt));
+		container.logger.info(`verboseLog cog - tickOneTwenty at ${thing.format('LT')}`);
+		getChannelAndSend(`Tick (2 hours): ${thing.format('LT')}`);
 	}
 }
