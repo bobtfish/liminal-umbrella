e388aee071534d94b890967aaa81d05e88348d40
Tomas Doran <t0m@the.internet.never.works>
2024-07-18 21:23:06 +0100

    Add edit link

--- frontend/src/dm/ViewGames.tsx
+++ frontend/src/dm/ViewGames.tsx
@@ -1,5 +1,7 @@
 import Table from 'antd/es/table';
 import Spin from 'antd/es/spin';
+import { EditOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 import { GameSchema, type GameListItem } from 'common/schema';
 import { getTableComponents, ColumnTypes, getListQueries, getColumns, DefaultColumns, WrapCRUD } from '../CRUD.js';
 import { isAdmin } from '../Auth.js';
@@ -10,6 +12,17 @@ export default function ViewGames() {
 	const { result, isMutating, handleSave } = getListQueries<GameListItem>('/api/gamesessions', 'gamesessions');
 
 	const defaultColumns: DefaultColumns = [
+		{
+			title: 'Edit',
+			dataIndex: 'edit',
+			render: (_foo, record) => {
+				return (
+					<Link to={`../viewgame/${record.key}`} relative="path">
+						<EditOutlined />
+					</Link>
+				);
+			}
+		},
 		{
 			title: 'Name',
 			dataIndex: 'name',
