57b24ab963e3607253ec6541db2689c8d84cdcc4
Tomas Doran <t0m@the.internet.never.works>
2024-07-19 01:21:34 +0100

    Add DM options to homepage

--- frontend/src/Homepage.tsx
+++ frontend/src/Homepage.tsx
@@ -1,12 +1,15 @@
 import List from 'antd/es/list';
+import { Link } from 'react-router-dom';
+import Button from 'antd/es/button';
 import { useContext } from 'react';
-import { AuthData, isAuthenticated } from './Auth';
+import { AuthData, isAuthenticated, isDM } from './Auth';
 import { DebugContext } from './Debug';
 import './App.css';
 
 function HomePage() {
 	const { debug } = useContext(DebugContext);
 	const auth = isAuthenticated();
+	const dm = isDM();
 	return (
 		<>
 			<div>
@@ -16,6 +19,22 @@ function HomePage() {
 					<br />
 					Please see the menus at the top to find your way around.
 				</div>
+				{dm ? (
+					<>
+						<Link to={`./dm/newgame`} relative="path">
+							<Button type="primary" style={{ marginBottom: 16 }}>
+								Create new game
+							</Button>
+						</Link>
+						<Link to={`./dm/viewgames`} relative="path">
+							<Button type="primary" style={{ marginBottom: 16 }}>
+								View games
+							</Button>
+						</Link>
+					</>
+				) : (
+					<></>
+				)}
 				<List header={<div>Your Roles:</div>} bordered dataSource={auth.roles} renderItem={(item: string) => <List.Item>{item}</List.Item>} />
 			</div>
 		</>
