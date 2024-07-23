20ec88f038dda908f14b2f2537de75e57d53adff
Tomas Doran <t0m@the.internet.never.works>
2024-07-23 09:52:36 +0100

    soft deletes don't play nice with the unique index

--- src/migrations/00022-plannedgames-fix.ts
+++ src/migrations/00022-plannedgames-fix.ts
@@ -1,73 +1,11 @@
-import { DataTypes } from '@sequelize/core';
 import type { MigrationParams } from 'umzug';
 
 export const up = async (uz: MigrationParams<any>) => {
 	const sq = uz.context.sequelize;
 	const qi = uz.context.sequelize.getQueryInterface();
 	await sq.transaction(async (transaction: any) => {
-		await qi.createTable(
-			'gamesessionusersignups',
-			{
-				key: {
-					type: DataTypes.INTEGER,
-					allowNull: false,
-					primaryKey: true
-				},
-				userKey: {
-					type: DataTypes.TEXT,
-					allowNull: false,
-					references: {
-						table: 'users',
-						key: 'key'
-					},
-					onDelete: 'RESTRICT',
-					onUpdate: 'CASCADE'
-				},
-				gameSessionKey: {
-					type: DataTypes.INTEGER,
-					allowNull: false,
-					references: {
-						table: 'gamesessions',
-						key: 'key'
-					},
-					onDelete: 'CASCADE',
-					onUpdate: 'CASCADE'
-				},
-				createdAt: {
-					type: DataTypes.TEXT,
-					allowNull: false
-				},
-				updatedAt: {
-					type: DataTypes.TEXT,
-					allowNull: false
-				}
-			},
-			{ transaction }
-		);
-		await qi.addIndex(
-			'gamesessionusersignups',
-			{
-				name: 'game_session_user_signups_game_session_key',
-				fields: ['gameSessionKey']
-			},
-			{ transaction }
-		);
-		await qi.addIndex(
-			'gamesessionusersignups',
-			{
-				name: 'game_session_user_signups_user_key',
-				fields: ['userKey'],
-				unique: true
-			},
-			{ transaction }
-		);
+		await qi.removeColumn('plannedGame', 'deletedAt', { transaction });
 	});
 };
 
-export const down = async (uz: MigrationParams<any>) => {
-	const sq = uz.context.sequelize;
-	const qi = uz.context.sequelize.getQueryInterface();
-	await sq.transaction(async (transaction: any) => {
-		await qi.dropTable('gamesessionusersignups', { transaction });
-	});
-};
+export const down = async (uz: MigrationParams<any>) => {};
