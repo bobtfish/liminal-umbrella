c44cbea179b2718ad39f835841ba53a3053ccb96
Tomas Doran <t0m@the.internet.never.works>
2024-07-23 09:53:13 +0100

    soft deletes don't play nice with the unique index

--- src/migrations/00022-plannedgames-fix.ts
+++ src/migrations/00022-plannedgames-fix.ts
@@ -4,7 +4,7 @@ export const up = async (uz: MigrationParams<any>) => {
 	const sq = uz.context.sequelize;
 	const qi = uz.context.sequelize.getQueryInterface();
 	await sq.transaction(async (transaction: any) => {
-		await qi.removeColumn('plannedGame', 'deletedAt', { transaction });
+		await qi.removeColumn('plannedgames', 'deletedAt', { transaction });
 	});
 };
 
