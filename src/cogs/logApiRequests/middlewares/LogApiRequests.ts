b2cadb5634592f4b0cc188146fcf9ac8f9cfca72
Tomas Doran <t0m@the.internet.never.works>
2024-07-30 23:49:00 +0100

    Add response code to HTTP log

--- src/cogs/logApiRequests/middlewares/LogApiRequests.ts
+++ src/cogs/logApiRequests/middlewares/LogApiRequests.ts
@@ -3,9 +3,9 @@ import { ApiRequest, ApiResponse } from '@sapphire/plugin-api';
 import { getChannelAndSend } from '../../verboseLog/utils.js';
 
 export class LogApiRequests extends Middleware {
-	public override run(request: ApiRequest, _response: ApiResponse) {
+	public override run(request: ApiRequest, response: ApiResponse) {
 		if (request.url == '/health' || request.url == '/metrics') return;
 		const user = request.auth ? request.auth.nickname : 'ANON';
-		getChannelAndSend(`API request - ${user}: ${request.method} ${request.url}`);
+		getChannelAndSend(`API request - ${user}: ${request.method} ${request.url} - ${response.statusCode}`);
 	}
 }
