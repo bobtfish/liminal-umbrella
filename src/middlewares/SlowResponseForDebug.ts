f176502b5435a4c30ad721686f96f216bed3bc83
Tomas Doran <t0m@the.internet.never.works>
2024-07-31 22:52:14 +0100

    Add an optional middleware useful for debugging loading UXes

---
+++ src/middlewares/SlowResponseForDebug.ts
@@ -0,0 +1,19 @@
+import { Middleware } from '@sapphire/plugin-api';
+import { ApiRequest, ApiResponse } from '@sapphire/plugin-api';
+
+export class SlowResponseForDebug extends Middleware {
+	public constructor(context: Middleware.LoaderContext, options: Middleware.Options = {} as Middleware.Options) {
+		super(context, options);
+		this.enabled = (process.env.NODE_ENV == 'development' && !!process.env.RPGBOT_SLOW_RESPONSE) || false;
+	}
+
+	public override async run(request: ApiRequest, _response: ApiResponse) {
+		const logger = this.container.logger;
+		await new Promise<void>((resolve, _reject) => {
+			logger.debug(`Slow: ${request.method} ${request.url}`);
+			setTimeout(() => {
+				resolve();
+			}, 10000);
+		});
+	}
+}
