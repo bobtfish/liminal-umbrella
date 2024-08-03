e10f95a74cbc707e49c5a9cbba4f515f204130af
Tomas Doran <t0m@the.internet.never.works>
2024-08-03 13:13:32 +0100

    We do not want a unique index here. Players sign up to more than one game

---
+++ src/migrations/00027-gamesessionusersignups-removeunique.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from '@sequelize/core';
+import type { MigrationParams } from 'umzug';
+
+export const up = async (uz: MigrationParams<any>) => {
+	const sq = uz.context.sequelize;
+	const qi = uz.context.sequelize.getQueryInterface();
+	await sq.transaction(async (transaction: any) => {
+		await qi.removeIndex(
+			'gamesessionusersignups', 'game_session_user_signups_user_key'
+		);
+		await qi.addIndex(
+			'gamesessionusersignups',
+			{
+				name: 'game_session_user_signups_user_key',
+				fields: ['userKey'],
+			},
+			{ transaction }
+		);
+	});
+};
+
+export const down = async (uz: MigrationParams<any>) => {
+};
