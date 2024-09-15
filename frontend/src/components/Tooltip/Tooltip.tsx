5994207513e5a63f21f2edf8cd3c4b5a4803597b
Tomas Doran <t0m@the.internet.never.works>
2024-09-15 20:50:32 +0100

    Tooltip becomes a component

--- frontend/src/components/Tooltip/Tooltip.tsx
+++ frontend/src/components/Tooltip/Tooltip.tsx
@@ -1,14 +1,15 @@
 import { useContext } from 'react';
-import { botMessage } from './components/BotMessage/BotMessage.js';
+import { useBotMessage } from '../../components/BotMessage';
 import AntdTooltip from 'antd/es/tooltip';
 import type { PresetColorType } from 'antd/es/_util/colors.js';
-import { DebugContext } from './Debug';
+import { DebugContext } from '../../Debug';
 
 export type TooltipParams = {
 	messageKey: string;
 	color?: PresetColorType | undefined;
 };
-export default function Tooltip({ messageKey, color, children }: TooltipParams & { children: React.ReactNode }) {
+export function Tooltip({ messageKey, color, children }: TooltipParams & { children: React.ReactNode }) {
+	const { botMessage } = useBotMessage();
 	const { debug } = useContext(DebugContext);
 	const title = botMessage(messageKey, debug ? undefined : null);
 	return title ? (
