a9dca1e813c001e9b20581a9e8afdef6682e07cc
Tomas Doran <t0m@the.internet.never.works>
2024-09-29 13:46:17 +0100

    More users

---
+++ src/migrations/00029-user-argh.ts
@@ -0,0 +1,13 @@
+import type { MigrationParams } from 'umzug';
+
+export const up = async (uz: MigrationParams<any>) => {
+	const sq = uz.context.sequelize;
+	//const qi = uz.context.sequelize.getQueryInterface();
+	await sq.transaction(async (transaction: any) => {
+        //await sq.query("DELETE FROM users WHERE username IN ('badbonepanda', 'yjen', '_chiae', 'generaldruzod', 'greatluxurysalt')", { raw: true, transaction });
+		await sq.query("DELETE FROM users WHERE username IN ('imtired', 'sorcha.flame.on', 'astroboy101.', 'sirprises', 'alycat604', 'doomsdaydevice', 'meownerva')", { raw: true, transaction });
+	});
+};
+
+export const down = async (_uz: MigrationParams<any>) => {
+};
