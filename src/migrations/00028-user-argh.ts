17fcfb328da1306a724c1d8bce9ca5fda999f620
Tomas Doran <t0m@the.internet.never.works>
2024-09-29 13:37:10 +0100

    Remove users that broke, to be re-added

---
+++ src/migrations/00028-user-argh.ts
@@ -0,0 +1,12 @@
+import type { MigrationParams } from 'umzug';
+
+export const up = async (uz: MigrationParams<any>) => {
+	const sq = uz.context.sequelize;
+	//const qi = uz.context.sequelize.getQueryInterface();
+	await sq.transaction(async (transaction: any) => {
+        await sq.query("DELETE FROM users WHERE username IN ('badbonepanda', 'yjen', '_chiae', 'generaldruzod', 'greatluxurysalt')", { raw: true, transaction });
+	});
+};
+
+export const down = async (_uz: MigrationParams<any>) => {
+};
