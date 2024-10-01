e4e63affb6926ae1d11367f0e1763a92bec27ae9
Tomas Doran <t0m@the.internet.never.works>
2024-10-01 12:07:10 +0100

    Actually send missing greetings every 2 hours

--- src/cogs/greetNewUsers/listeners/eventTickOneTwenty.ts
+++ src/cogs/greetNewUsers/listeners/eventTickOneTwenty.ts
@@ -1,53 +1,49 @@
 import { Listener, container } from '@sapphire/framework';
 import { TickFive } from '../../../lib/events/index.js';
-import { Sequential, shortSleep } from '../../../lib/utils.js';
+import { Sequential, shortSleep, sleepUpToTwoHours } from '../../../lib/utils.js';
 import { User } from '../../../lib/database/model.js';
 import { Op } from '@sequelize/core';
 import { CustomEvents } from '../../../lib/events.js';
 import { doUserGreeting } from '../utils.js';
 
 export class greetNewUsersTickOneTwentyListener extends Listener {
-	public constructor(context: Listener.LoaderContext, options: Listener.Options) {
-		super(context, {
-			...options,
-			name: 'greetNewUsersTickOneTwentyListener',
-			emitter: container.events,
-			event: CustomEvents.TickFive
-		});
-	}
+    public constructor(context: Listener.LoaderContext, options: Listener.Options) {
+        super(context, {
+            ...options,
+            name: 'greetNewUsersTickOneTwentyListener',
+            emitter: container.events,
+            event: CustomEvents.TickOneTwenty
+        });
+    }
 
-	async run(_e: TickFive) {
-		//await sleepUpToTwoHours();
-		this.container.logger.info('Doing greetNewUsers find now');
-		await this.doMissedGreetings();
-		this.container.logger.info('Finished greetNewUsers find');
-	}
+    async run(_e: TickFive) {
+        await sleepUpToTwoHours();
+        await this.doMissedGreetings();
+    }
 
-	async doMissedGreetings() {
-		const missedGreetings = await this.findMissedGreetings();
-		this.container.logger.info(`Found ${missedGreetings.length} misssed greetings to send`);
-		for (const user of missedGreetings) {
-			await this.sendMissedGreeting(user);
-			await shortSleep();
-		}
-	}
+    async doMissedGreetings() {
+        const missedGreetings = await this.findMissedGreetings();
+        this.container.logger.info(`Found ${missedGreetings.length} misssed greetings to send`);
+        for (const user of missedGreetings) {
+            await this.sendMissedGreeting(user);
+            await shortSleep();
+        }
+    }
 
-	@Sequential
-	findMissedGreetings() {
-		return User.findAll({
-			include: ['greetingMessage'],
-			where: {
-				left: false,
-				bot: false,
-				'$greetingMessage.userId$': { [Op.eq]: null }
-			}
-		});
-	}
+    @Sequential
+    findMissedGreetings() {
+        return User.findAll({
+            include: ['greetingMessage'],
+            where: {
+                left: false,
+                bot: false,
+                '$greetingMessage.userId$': { [Op.eq]: null }
+            }
+        });
+    }
 
-	@Sequential
-	async sendMissedGreeting(user: User) {
-		//const db = await this.container.database.getdb();
-		this.container.logger.info(`Send greeting to ${user.nickname}`);
-		await doUserGreeting(user);
-	}
+    @Sequential
+    async sendMissedGreeting(user: User) {
+        await doUserGreeting(user);
+    }
 }
