94251a0b1297b1f45fe1d0f30b68c8002385ad07
Tomas Doran <t0m@the.internet.never.works>
2024-10-02 17:16:30 +0100

    Fixes

--- src/cogs/kickNewUsersOnX/listeners/eventMessageReactionAdd.ts
+++ src/cogs/kickNewUsersOnX/listeners/eventMessageReactionAdd.ts
@@ -1,33 +1,33 @@
 import { Listener, Events } from '@sapphire/framework';
 import { MessageReaction } from 'discord.js';
-import { GreetingMessage } from '../../../lib/database/model.js';
-import { getMessage } from '../../../lib/message.js';
-import { getGuildMemberById } from '../../../lib/discord.js';
+//import { GreetingMessage } from '../../../lib/database/model.js';
+//import { getMessage } from '../../../lib/message.js';
+//import { getGuildMemberById } from '../../../lib/discord.js';
 import { Sequential } from '../../../lib/utils.js';
 
 export class verboseLogBotStartedListener extends Listener {
-	public constructor(context: Listener.LoaderContext, options: Listener.Options) {
-		super(context, {
-			...options,
-			name: 'kickNewUsersOnX',
-			event: Events.MessageReactionAdd
-		});
-	}
+    public constructor(context: Listener.LoaderContext, options: Listener.Options) {
+        super(context, {
+            ...options,
+            name: 'kickNewUsersOnX',
+            event: Events.MessageReactionAdd
+        });
+    }
 
-	@Sequential
-	async run(r: MessageReaction) {
-		const db = await this.container.database.getdb();
-		if (r.partial) {
-			try {
-				await r.fetch();
-			} catch (error) {
-				this.container.logger.error('Could not fetch reaction: ', error);
-				return;
-			}
-		}
-		console.log('Emoji name: ', r.emoji.name);
-		return;
-		/*
+    @Sequential
+    async run(r: MessageReaction) {
+        //const db = await this.container.database.getdb();
+        if (r.partial) {
+            try {
+                await r.fetch();
+            } catch (error) {
+                this.container.logger.error('Could not fetch reaction: ', error);
+                return;
+            }
+        }
+        console.log('Emoji name: ', r.emoji.name);
+        return;
+        /*
         const messageId = r.message.id;
         const greeting = await GreetingMessage.findOne({ include: ['user'], where: { messageId } });
         if (!greeting) return;
@@ -42,5 +42,5 @@ export class verboseLogBotStartedListener extends Listener {
         });
         this.container.logger.info('verboseLog cog - botStarted');
         */
-	}
+    }
 }
