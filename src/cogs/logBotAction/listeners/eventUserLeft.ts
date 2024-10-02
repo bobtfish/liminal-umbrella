2dcfa6fee01db5f77411cb671041044c3f11d1be
Tomas Doran <t0m@the.internet.never.works>
2024-10-02 16:23:03 +0100

    Cleanup user left

--- src/cogs/logBotAction/listeners/eventUserLeft.ts
+++ src/cogs/logBotAction/listeners/eventUserLeft.ts
@@ -5,25 +5,28 @@ import { userMention, EmbedBuilder } from 'discord.js';
 import { CustomEvents } from '../../../lib/events.js';
 
 export class logBotActionUserLeftListener extends Listener {
-	public constructor(context: Listener.LoaderContext, options: Listener.Options) {
-		super(context, {
-			...options,
-			name: 'logBotActionUserLeft',
-			emitter: container.events,
-			event: CustomEvents.UserLeft
-		});
-	}
-	async run(e: UserLeft) {
-		// Log user left (with nickname in bot_log)
-		const exampleEmbed = new EmbedBuilder()
-			.setColor(0xff0000)
-			.setAuthor({ name: 'Member Left', iconURL: e.avatarURL })
-			.setDescription(userMention(e.id))
-			.setThumbnail(e.avatarURL)
-			.addFields({ name: 'Account name', value: e.username, inline: true }, { name: 'Nickname', value: e.nickname, inline: true })
-			.setTimestamp()
-			.setFooter({ text: `ID: ${e.id}` });
+    public constructor(context: Listener.LoaderContext, options: Listener.Options) {
+        super(context, {
+            ...options,
+            name: 'logBotActionUserLeft',
+            emitter: container.events,
+            event: CustomEvents.UserLeft
+        });
+    }
+    async run(e: UserLeft) {
+        // Log user left (with nickname in bot_log)
+        const embed = new EmbedBuilder()
+            .setColor(0xff0000)
+            .setAuthor({ name: 'Member Left', iconURL: e.avatarURL })
+            .setDescription(userMention(e.id))
+            .setThumbnail(e.avatarURL)
+            .addFields(
+                { name: 'Account name', value: e.username, inline: true },
+                { name: 'Nickname', value: e.nickname, inline: true }
+            )
+            .setTimestamp()
+            .setFooter({ text: `ID: ${e.id}` });
 
-		await getChannelAndEmbed(exampleEmbed);
-	}
+        await getChannelAndEmbed(embed);
+    }
 }
