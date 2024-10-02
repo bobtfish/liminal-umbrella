815482f900507713398cde264af6bbbdfad478f0
Tomas Doran <t0m@the.internet.never.works>
2024-10-02 15:31:04 +0100

    Simplify message indexing

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -2,21 +2,21 @@ import { Listener, Events } from '@sapphire/framework';
 import { Message } from 'discord.js';
 import { Sequential } from '../lib/utils.js';
 
-export class MessageUpdateEvent extends Listener {
+export class MessageCreateEvent extends Listener {
     constructor(context: Listener.LoaderContext, options: Listener.Options) {
         super(context, {
             ...options,
-            event: Events.MessageUpdate
+            event: Events.MessageCreate
         });
     }
 
     @Sequential
-    public override run(_: Message, newMessage: Message) {
-        if (!newMessage.guildId) {
+    public override run(message: Message) {
+        if (!message.guildId) {
             return;
         }
-        console.log('messageUpdate event index message', newMessage.id);
-        this.container.database.indexMessage(newMessage);
+        console.log('messageCreate event index message', message.id);
+        this.container.database.indexMessage(message);
         return;
     }
 }
---
+++ src/listeners/messageDelete.ts
@@ -0,0 +1,22 @@
+import { Listener, Events } from '@sapphire/framework';
+import { Message } from 'discord.js';
+import { Sequential } from '../lib/utils.js';
+
+export class MessageCreateEvent extends Listener {
+	constructor(context: Listener.LoaderContext, options: Listener.Options) {
+		super(context, {
+			...options,
+			event: Events.MessageDelete
+		});
+	}
+
+	@Sequential
+	public override run(message: Message) {
+		if (!message.guildId) {
+			return;
+		}
+		console.log('messageDelete event delete message', message.id);
+		this.container.database.deleteMessage(message);
+		return;
+	}
+}
