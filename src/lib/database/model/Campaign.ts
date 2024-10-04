ca5d441f3c43b9112dfd058aec34549054578d64
Tomas Doran <t0m@the.internet.never.works>
2024-10-04 19:00:20 +0100

    Slight cleanups and fixes

--- src/lib/database/model/Campaign.ts
+++ src/lib/database/model/Campaign.ts
@@ -3,41 +3,40 @@
 import { DataTypes, Model, InferAttributes, InferCreationAttributes, NonAttribute, BelongsToGetAssociationMixin } from '@sequelize/core';
 import { Attribute, BelongsTo, NotNull, PrimaryKey, Unique } from '@sequelize/core/decorators-legacy';
 import User from './User.js';
-import Role from './Role.js';
+//import Role from './Role.js';
 import GameSystem from './GameSystem.js';
 
 export default class Campaign extends Model<InferAttributes<Campaign>, InferCreationAttributes<Campaign>> {
-    @Attribute(DataTypes.STRING)
-    @NotNull
-    @PrimaryKey
-    declare key: string;
+	@Attribute(DataTypes.STRING)
+	@NotNull
+	@PrimaryKey
+	declare key: string;
 
-    @Attribute(DataTypes.STRING)
-    @NotNull
-    declare name: string;
+	@Attribute(DataTypes.STRING)
+	@NotNull
+	declare name: string;
 
-    @Attribute(DataTypes.STRING)
-    @NotNull
-    declare dmUser: string;
+	@Attribute(DataTypes.STRING)
+	@NotNull
+	declare dmUser: string;
 
-    @BelongsTo(() => User, 'dmUser')
-    declare dmUserOb?: NonAttribute<User>;
-    declare getDmUserOb: BelongsToGetAssociationMixin<User>;
+	@BelongsTo(() => User, 'dmUser')
+	declare dmUserOb?: NonAttribute<User>;
+	declare getDmUserOb: BelongsToGetAssociationMixin<User>;
 
-    @Attribute(DataTypes.STRING)
-    @Unique
-    @NotNull
-    declare role: string;
+	@Attribute(DataTypes.STRING)
+	@Unique
+	@NotNull
+	declare roleKey: string;
 
-    @BelongsTo(() => Role, 'role')
-    declare roleOb?: NonAttribute<Role>;
-    declare getRoleOb: BelongsToGetAssociationMixin<Role>;
+	//@BelongsTo(() => Role, 'role')
+	//declare roleOb?: NonAttribute<Role>;
+	//declare getRoleOb: BelongsToGetAssociationMixin<Role>;
 
-    @Attribute(DataTypes.STRING)
-    @NotNull
-    declare gamesystem: string;
+	@Attribute(DataTypes.STRING)
+	@NotNull
+	declare gamesystemKey: string;
 
-    @BelongsTo(() => GameSystem, 'gamesystem')
-    declare gamesystemOb?: NonAttribute<GameSystem>;
-    declare getGamesystemOb: BelongsToGetAssociationMixin<GameSystem>;
+	declare gamesystem?: NonAttribute<GameSystem>;
+	declare getGamesystem: BelongsToGetAssociationMixin<GameSystem>;
 }
