51731ceaebd81ac2a999373e91525247d90a20db
Tomas Doran <t0m@the.internet.never.works>
2024-10-09 12:29:28 +0100

    Spacing

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { MySapphireClient } from './sapphire.js';
 const client = new MySapphireClient();
 
 const main = async () => {
-	try {
-		await client.login(process.env.DISCORD_BOT_TOKEN);
-	} catch (error) {
-		client.logger.fatal(error);
-		await client.destroy();
-		process.exit(1);
-	}
+    try {
+        await client.login(process.env.DISCORD_BOT_TOKEN);
+    } catch (error) {
+        client.logger.fatal(error);
+        await client.destroy();
+        process.exit(1);
+    }
 };
 
 void main();
