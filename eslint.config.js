b130c701c77d7f2051d852aed9b0f1a0bcc68ee8
Tomas Doran <t0m@the.internet.never.works>
2024-10-09 13:25:22 +0100

    Fix more/harder

--- eslint.config.js
+++ eslint.config.js
@@ -37,44 +37,44 @@ export default [
           "modifiers": ["const", "global"],
           "format": ["UPPER_CASE"]
         }
-      ]
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          "selector": [
+            "parameter",
+            "variable"
+          ],
+          "leadingUnderscore": "require",
+          "format": ["camelCase"],
+          "modifiers": [
+            "unused"
+          ]
+        },
+        {
+          "selector": [
+            "parameter",
+            "variable"
+          ],
+          "leadingUnderscore": "allowDouble",
+          "format": [
+            "camelCase"
+          ]
+        }
+      ],
+      // note you must disable the base rule
+      // as it can report incorrect errors
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ],
     },
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        "selector": [
-          "parameter",
-          "variable"
-        ],
-        "leadingUnderscore": "require",
-        "format": ["camelCase"],
-        "modifiers": [
-          "unused"
-        ]
-      },
-      {
-        "selector": [
-          "parameter",
-          "variable"
-        ],
-        "leadingUnderscore": "allowDouble",
-        "format": [
-          "camelCase"
-        ]
-      }
-    ],
-    // note you must disable the base rule
-    // as it can report incorrect errors
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        "args": "all",
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_",
-        "caughtErrorsIgnorePattern": "^_"
-      }
-    ],
   },
   {
     files: ['**/*.js', 'eslint.config.mjs', 'jest.config.cjs'],
--- src/lib/parsers.ts
+++ src/lib/parsers.ts
@@ -1,63 +1,63 @@
 export interface AGameSession {
-	name: string;
-	type: string;
-	date: string;
-	location: string;
-	spaces_total: number;
-	spaces_free: number;
-	dm_contact: string;
-	description: string;
+    name: string;
+    type: string;
+    date: string;
+    location: string;
+    spaces_total: number;
+    spaces_free: number;
+    dm_contact: string;
+    description: string;
 }
 
 const inputRe = new RegExp(/\s\s\s+/);
-const nameRe = new RegExp(/(?:\*\*)?(?:Adventure [nN]ame|Game)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
-const typeRe = new RegExp(/(?:\*\*)?(?:Type of Adventure)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
+const nameRe = new RegExp(/(?:\*\*)?(?:Adventure [nN]ame|Game)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n*]*)/);
+const typeRe = new RegExp(/(?:\*\*)?(?:Type of Adventure)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n*]*)/);
 const dateRe = new RegExp(/(?:\*\*)?(?:(?:Start )?[dD]ate)\s*(?:\*\*)?:\s*(?:\*\*)?\s*(?:\w+day)?\s*(\d{1,2}\/\d{1,2}\/\d{2,4})/);
-const dmRe = new RegExp(/(?:\*\*)?(?:DM [cC]ontact|DM)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
-const locationRe = new RegExp(/(?:\*\*)?Location\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n\*]*)/);
+const dmRe = new RegExp(/(?:\*\*)?(?:DM [cC]ontact|DM)\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n*]*)/);
+const locationRe = new RegExp(/(?:\*\*)?Location\s*(?:\*\*)?:\s*(?:\*\*)?\s*([^\n*]*)/);
 const spacesRe = new RegExp(/(?:\*\*)?Spaces [Cc]urrently [Aa]vailable\s*(?:\*\*)?:\s*(?:\*\*)?\s*(\d)(?:\/| of )(\d)/);
 const descriptionRe = new RegExp(/(?:\*\*)?(?:Brief )?[Dd]escription\s*?(?:\*\*)?:?\s*?(?:\*\*)?\s*(.*)$/s);
 
 export function parseAvailableGame(input: string): AGameSession {
-	input = input.replace(inputRe, '\n');
-	const out = {
-		name: '??',
-		type: '??',
-		date: '??',
-		location: '??',
-		spaces_total: 0,
-		spaces_free: 0,
-		dm_contact: '??',
-		description: '??'
-	};
-	const nameMatches = nameRe.exec(input);
-	if (nameMatches) {
-		out.name = nameMatches[1];
-	}
-	const typeMatches = typeRe.exec(input);
-	if (typeMatches) {
-		out.type = typeMatches[1];
-	}
-	const dateMatches = dateRe.exec(input);
-	if (dateMatches) {
-		out.date = dateMatches[1];
-	}
-	const dmMatches = dmRe.exec(input);
-	if (dmMatches) {
-		out.dm_contact = dmMatches[1];
-	}
-	const locationMatches = locationRe.exec(input);
-	if (locationMatches) {
-		out.location = locationMatches[1];
-	}
-	const spacesMatches = spacesRe.exec(input);
-	if (spacesMatches) {
-		out.spaces_free = parseInt(spacesMatches[1]);
-		out.spaces_total = parseInt(spacesMatches[2]);
-	}
-	const descriptionMatches = descriptionRe.exec(input);
-	if (descriptionMatches) {
-		out.description = descriptionMatches[1].trim();
-	}
-	return out;
+    input = input.replace(inputRe, '\n');
+    const out = {
+        name: '??',
+        type: '??',
+        date: '??',
+        location: '??',
+        spaces_total: 0,
+        spaces_free: 0,
+        dm_contact: '??',
+        description: '??'
+    };
+    const nameMatches = nameRe.exec(input);
+    if (nameMatches) {
+        out.name = nameMatches[1];
+    }
+    const typeMatches = typeRe.exec(input);
+    if (typeMatches) {
+        out.type = typeMatches[1];
+    }
+    const dateMatches = dateRe.exec(input);
+    if (dateMatches) {
+        out.date = dateMatches[1];
+    }
+    const dmMatches = dmRe.exec(input);
+    if (dmMatches) {
+        out.dm_contact = dmMatches[1];
+    }
+    const locationMatches = locationRe.exec(input);
+    if (locationMatches) {
+        out.location = locationMatches[1];
+    }
+    const spacesMatches = spacesRe.exec(input);
+    if (spacesMatches) {
+        out.spaces_free = parseInt(spacesMatches[1]);
+        out.spaces_total = parseInt(spacesMatches[2]);
+    }
+    const descriptionMatches = descriptionRe.exec(input);
+    if (descriptionMatches) {
+        out.description = descriptionMatches[1].trim();
+    }
+    return out;
 }
