ccd2f922432a8ac6f066d45c8f392240c9dc1642
Tomas Doran <t0m@the.internet.never.works>
2024-10-09 22:37:42 +0100

    Fix new user greeting, again

---
+++ src/migrations/00046-botmessage.ts
@@ -0,0 +1,15 @@
+import type { MigrationParams } from 'umzug';
+import { BotMessage } from '../lib/database/model.js';
+
+export const up = async (_uz: MigrationParams<any>) => {
+    await BotMessage.findOne({ where: { name: 'NEW_USER_GREETING' } }).then((message) => {
+        if (!message) {
+            return;
+        }
+        message.value =
+            '<@${this.u.key}> Welcome to Preston RPG Community.  If you have read the rules and altered your name as asked then an Admin will be along shortly to give access to the rest of the server.';
+        message.save();
+    });
+};
+
+export const down = async (_uz: MigrationParams<any>) => {};
