0e6d17aff0c0a063e771726d71d1bd1b89d62872
Tomas Doran <t0m@the.internet.never.works>
2024-10-10 22:24:25 +0100

    Move migration

--- frontend/src/Pages/admin/Users.tsx
+++ frontend/src/Pages/admin/Users.tsx
@@ -21,7 +21,7 @@ interface RoleFragment {
     position: number;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/naming-convention
 const RenderNickname = ({nickname, record}: {nickname: string, record: any}) => {
     const uf = record as UserFragment;
     const roles = uf.roles
@@ -40,6 +40,7 @@ const RenderNickname = ({nickname, record}: {nickname: string, record: any}) => 
     );
 }
 
+// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-explicit-any
 const RenderLastSeen = ({lastSeenTime, record}: {lastSeenTime: string, record: any}) => {
     const lst = new Date(lastSeenTime);
     if (lst <= new Date('1970-01-02:00:00:00.000Z')) {
@@ -47,13 +48,19 @@ const RenderLastSeen = ({lastSeenTime, record}: {lastSeenTime: string, record: a
     }
     const seen = <><ReactTimeAgo date={lst} /> ({lst.toLocaleDateString()})</>;
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const lastSeenChannelName: string = record.lastSeenChannelName;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const lastSeenLink: string = record.lastSeenLink;
+
     return <>
             {seen}
-            {record.lastSeenChannelName ? <>&nbsp;in #{record.lastSeenChannelName}</> : null}
-            {record.lastSeenChannel ? <>&nbsp;<a href={record.lastSeenLink} target="_blank"><LinkOutlined /></a></> : null}
+            {lastSeenChannelName ? <>&nbsp;in #{lastSeenChannelName}</> : null}
+            {lastSeenLink ? <>&nbsp;<a href={lastSeenLink} target="_blank"><LinkOutlined /></a></> : null}
         </>
 }
 
+// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-explicit-any
 const RenderRoles = ({roles, record}: {roles: { name: string; hexColor: string }[], record: any}) =>
     roles
         .filter((role) => role.name !== '@everyone' && role.name !== 'AllUsers')
@@ -63,6 +70,7 @@ const RenderRoles = ({roles, record}: {roles: { name: string; hexColor: string }
             return 0;
         })
         .map((role, index) => (
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             <span key={`${record.key}${index}`}>
                 <Tag color={role.hexColor == '#000000' ? undefined : role.hexColor}>{role.name}</Tag>
                 <br />
@@ -81,6 +89,7 @@ export function AdminUsers() {
 
     console.log(`lastSeenFilter: ${lastSeenFilter}`);
 
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const FilterLastSeen = ({setSelectedKeys, selectedKeys, confirm, clearFilters, close}: FilterDropdownProps) => {
         return (
             <div style={{ padding: 8 }} onKeyDown={(e) => { e.stopPropagation(); }}>
@@ -140,6 +149,7 @@ export function AdminUsers() {
             editable: false,
             ellipsis: true,
             defaultSortOrder: 'ascend',
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
             sorter: (a, b) => a.nickname.toUpperCase() < b.nickname.toUpperCase() ? -1 : a.nickname.toUpperCase() > b.nickname.toUpperCase() ? 1 : 0,
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             render: (nickname: string, record: any) => <RenderNickname nickname={nickname} record={record as UserListItem} />
@@ -149,6 +159,7 @@ export function AdminUsers() {
             dataIndex: 'name',
             editable: false,
             ellipsis: true,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
             sorter: (a, b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : a.name.toUpperCase() > b.name.toUpperCase() ? 1 : 0,
         },
         {
@@ -156,6 +167,7 @@ export function AdminUsers() {
             dataIndex: 'username',
             editable: false,
             ellipsis: true,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
             sorter: (a, b) => a.username.toUpperCase() < b.username.toUpperCase() ? -1 : a.username.toUpperCase() > b.username.toUpperCase() ? 1 : 0,
 
         },
@@ -163,10 +175,12 @@ export function AdminUsers() {
             title: 'Last seen',
             dataIndex: 'lastSeenTime',
             editable: false,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             render: (lastSeenTime, record) => <RenderLastSeen lastSeenTime={lastSeenTime} record={record} />,
             sorter: (a, b) => a.lastSeenTime < b.lastSeenTime ? -1 : a.lastSeenTime > b.lastSeenTime ? 1 : 0,
             filterDropdown: (props: FilterDropdownProps) => <FilterLastSeen {...props} />,
             onFilter: (value, record) => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                 const recordTime = (new Date(record.lastSeenTime)).getTime();
                 const valTime = Date.now() - Math.abs(value as number * 365/12 * 24 * 60 * 60 * 1000);
                 console.log(`Record last seen ${recordTime} val ${valTime} answer ${recordTime < valTime}`);
@@ -179,9 +193,12 @@ export function AdminUsers() {
             dataIndex: 'roles',
             editable: false,
             ellipsis: true,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             render: (roles, record) => <RenderRoles roles={roles} record={record} />,
             onFilter: (value, record) => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
                 const roleNames = record.roles.map((role: { name: any; }) => role.name);
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
                 return value === 'Not Known Member' ? !roleNames.includes('Known Member') : roleNames.includes('Known Member')
             },
             filterMultiple: true,
