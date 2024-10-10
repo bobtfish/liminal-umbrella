a0b6b2f02176c2b50439d591d3da8207cedce7e1
Tomas Doran <t0m@the.internet.never.works>
2024-10-10 22:52:11 +0100

    Unfuck user more

---
+++ src/migrations/00049-unfuck-user.ts
@@ -0,0 +1,13 @@
+import { DataTypes } from '@sequelize/core';
+import type { MigrationParams } from 'umzug';
+
+export const up = async (uz: MigrationParams<any>) => {
+    const sq = uz.context.sequelize;
+    await sq.transaction(async (transaction: any) => {
+      await sq.query('UPDATE messages SET id = "1293940001033424948" WHERE id = "1289929489769562112"', { raw: true, transaction });
+      await sq.query('UPDATE greetingmessages SET messageId = "1293940001033424948" WHERE userId = "417047110479773707"', { raw: true, transaction });
+    })
+};
+
+export const down = async (uz: MigrationParams<any>) => {
+};
