e06f7841bd6a1012f1143a7d8b479fc7ffb2af9c
Tomas Doran <t0m@the.internet.never.works>
2024-10-13 21:11:03 +0100

    Comma

--- src/migrations/00053-threads-fkey.ts
+++ src/migrations/00053-threads-fkey.ts
@@ -5,7 +5,7 @@ export const up = async (uz: MigrationParams<any>) => {
     await sq.transaction(async (transaction: any) => {
       await sq.query('ALTER TABLE threads RENAME TO _threads_old', { raw: true, transaction });
       await sq.query(
-	  'CREATE TABLE "threads" (`key` TEXT NOT NULL UNIQUE PRIMARY KEY, `name` TEXT NOT NULL, `parentId` TEXT NOT NULL REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT, `archived` INTEGER NOT NULL, `archiveTimestamp` INTEGER NOT NULL, `locked` INTEGER NOT NULL, `createdTimestamp` INTEGER NOT NULL, `lastMessageId` TEXT, `lastSeenIndexedToDate` TEXT, `createdAt` TEXT NOT NULL, `updatedAt` TEXT NOT NULL)'
+	  'CREATE TABLE "threads" (`key` TEXT NOT NULL UNIQUE PRIMARY KEY, `name` TEXT NOT NULL, `parentId` TEXT NOT NULL REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT, `archived` INTEGER NOT NULL, `archiveTimestamp` INTEGER NOT NULL, `locked` INTEGER NOT NULL, `createdTimestamp` INTEGER NOT NULL, `lastMessageId` TEXT, `lastSeenIndexedToDate` TEXT, `createdAt` TEXT NOT NULL, `updatedAt` TEXT NOT NULL)',
           { raw: true, transaction }
       );
       await sq.query('INSERT INTO threads SELECT * FROM _threads_old', { raw: true, transaction });
