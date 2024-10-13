33bd4c01dd6d8177e7ab11d0a9469aa4b6cb8a4f
Tomas Doran <t0m@the.internet.never.works>
2024-10-13 21:10:26 +0100

    Fix fkey

---
+++ src/migrations/00053-threads-fkey.ts
@@ -0,0 +1,17 @@
+import type { MigrationParams } from 'umzug';
+
+export const up = async (uz: MigrationParams<any>) => {
+    const sq = uz.context.sequelize;
+    await sq.transaction(async (transaction: any) => {
+      await sq.query('ALTER TABLE threads RENAME TO _threads_old', { raw: true, transaction });
+      await sq.query(
+	  'CREATE TABLE "threads" (`key` TEXT NOT NULL UNIQUE PRIMARY KEY, `name` TEXT NOT NULL, `parentId` TEXT NOT NULL REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT, `archived` INTEGER NOT NULL, `archiveTimestamp` INTEGER NOT NULL, `locked` INTEGER NOT NULL, `createdTimestamp` INTEGER NOT NULL, `lastMessageId` TEXT, `lastSeenIndexedToDate` TEXT, `createdAt` TEXT NOT NULL, `updatedAt` TEXT NOT NULL)'
+          { raw: true, transaction }
+      );
+      await sq.query('INSERT INTO threads SELECT * FROM _threads_old', { raw: true, transaction });
+      await sq.query('DROP TABLE  _threads_old', { raw: true, transaction });
+    });
+    await sq.query('VACUUM');
+};
+
+export const down = async (_uz: MigrationParams<any>) => {};
